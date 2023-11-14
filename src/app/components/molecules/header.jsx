ee8be862c86c9a03f55aefc4cb0c239f4ac3b648
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2023-11-14 16:09:17 -0300

    header animation

--- src/app/components/molecules/header.jsx
+++ src/app/components/molecules/header.jsx
@@ -1,18 +1,40 @@
+"use client";
+
 import Link from "next/link";
 import { Logo } from "../atoms/logo";
+import { useEffect, useState } from "react";
 
 export function Header(props) {
   const { links = ["about", "experience", "skills", "projects"] } = props;
 
+  const [small, setSmall] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => {
+      if (e.target.scrollingElement.scrollTop > 400 && !small) {
+        setSmall(true);
+      } else if (e.target.scrollingElement.scrollTop <= 400 && small) {
+        setSmall(false);
+      }
+    });
+  });
+
   return (
     <header
-      className="fixed top-0 z-10 bg-transparent w-screen rounded-s-lg"
+      className="fixed top-0 z-10 bg-transparent w-screen rounded-b-xl"
       style={{
-        backdropFilter: "blur(4px)",
+        backdropFilter: small ? "blur(6px)" : "blur(4px)",
         boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+        height: small ? "45px" : "unset",
       }}
     >
-      <nav className="p-4 flex items-center justify-between">
+      <nav
+        className="p-4 flex items-center justify-between"
+        style={{
+          transform: small ? "scale(0.8) translate(0, -20%)" : "scale(1)",
+          transition: "all 0.4s",
+        }}
+      >
         <div className="mr-6">
           <Logo />
         </div>
